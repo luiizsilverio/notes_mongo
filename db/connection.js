e2582041d955c164a2a10238d3237d7160d7c73f
luiizsilverio <luiiz.silverio@gmail.com>
2022-02-20 17:31:06 -0300

    conexão com MongoDB Atlas

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,12 @@
 const {MongoClient} = require('mongodb')
 
 // notesDb é o nome do banco de dados
-const url = "mongodb://localhost:27017/notesDb"
+
+let url = "mongodb://localhost:27017/notesDb"
+
+if (process.env.DB_ATLAS === 'Yes') { // banco no MongoDB Atlas
+  url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5cmwr.mongodb.net/notesDb?retryWrites=true&w=majority`
+}
 
 let _db;
 
